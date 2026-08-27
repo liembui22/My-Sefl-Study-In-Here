9252b942f07f9e8f80f6977cf20072b002ecd5a9
Bùi Thanh Liêm <liembui135790@gmail.com>
2026-08-27 18:38:17 +0700

    end project 11

--- 2026.08.18 - 20_Project_HTMLCSSJS/Project_11_To_Do_List/index.js
+++ 2026.08.18 - 20_Project_HTMLCSSJS/Project_11_To_Do_List/index.js
@@ -2,14 +2,57 @@ const inputEl = document.querySelector('.input')
 const formEl = document.querySelector('.form')
 const ulEl = document.querySelector(".list")
 
+let list = JSON.parse(localStorage.getItem("list")) || []
+list.forEach(task =>{
+    toDoList(task)
+})
+
 formEl.addEventListener('submit',(event)=>{
     event.preventDefault();
     toDoList()
 });
 
-function toDoList(){
+function toDoList(task){
     let newTask = inputEl.value;
+    if (task){
+        newTask = task.name
+    }
     const liEl = document.createElement("li");
     liEl.innerText = newTask;
     ulEl.appendChild(liEl)
+    inputEl.value =""
+
+    const checkBtnEl = document.createElement("div")
+    checkBtnEl.innerHTML = `
+    <i class="fa-solid fa-square-check">
+    `;
+    liEl.appendChild(checkBtnEl)
+
+    const trashBtnEl = document.createElement("div")
+    trashBtnEl.innerHTML = `
+    <i class="fa-solid fa-trash">
+    `;
+    liEl.appendChild(trashBtnEl)
+
+    checkBtnEl.addEventListener('click',()=>{
+        liEl.classList.toggle("checked")
+        updateLocalStorage()
+    })
+    trashBtnEl.addEventListener('click',()=>{
+        liEl.remove();
+        updateLocalStorage()
+    })
+    updateLocalStorage()
+}
+
+function updateLocalStorage(){
+    const liEls = document.querySelectorAll('li')
+    list = []
+    liEls.forEach(liEl => {
+        list.push({
+            name: liEl.innerText,
+            checked: liEl.classList.contains("checked")
+        })
+    })
+    localStorage.setItem('list', JSON.stringify(list))
 }
